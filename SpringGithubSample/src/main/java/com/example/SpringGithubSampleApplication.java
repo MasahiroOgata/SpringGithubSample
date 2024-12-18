133bc5e64db0bb8a5520f475eb13c471d8d42dd5
user <user@DESKTOP-MFFK010>
2024-12-18 15:04:56 +0900

    repository作成

---
+++ SpringGithubSample/src/main/java/com/example/SpringGithubSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubSampleApplication.class, args);
+	}
+
+}
